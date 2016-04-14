3ec790373e225d5626cb5a03718c57f6507cb4d0
Neil Hoang <ottohoang@gmail.com>
2016-04-14 14:39:27 -0500

    final!
    It works.

--- formvalidation.js
+++ formvalidation.js
@@ -1,14 +1,19 @@
 function checkempty() {
 	var errormessage = "";
-	if(document.getElementById('name').value == ""){
+	var name = document.getElementById('name').value;
+	var company = document.getElementById('company').value;
+	var email = document.getElementById('email').value;
+	var phoneNum = document.getElementById('phoneNumber').value;
+	var website = document.getElementById('website').value;
+	if(name == ""){
 		errormessage += "Enter your Name \n";
 		document.getElementById('name').style.borderColor = "red";
 	}
-	if(document.getElementById('email').value == ""){
+	if(email == ""){
 		errormessage += "Enter your email \n";
 		document.getElementById('email').style.borderColor = "red";
 	}
-	if(document.getElementById('website').value == ""){
+	if(website == ""){
 		errormessage += "Enter your website \n";
 		document.getElementById('website').style.borderColor = "red";
 	}
@@ -17,14 +22,35 @@ function checkempty() {
 		return false;
 
 	}
+
+	if(!validateEmail(email)){
+		errormessage +="Email address is not valid \n";
+		document.getElementById('email').style.borderColor = "red";
+	}
+	if(!validatePhoneNumber(phoneNum)){
+		errormessage += "Phone number is not valid \n";
+		document.getElementById('phoneNumber').style.borderColor = "red";
+	}
+	if(!validateWebsite(website)){
+		errormessage += "Website is not valid \n";
+		document.getElementById('website').style.borderColor = "red";
+
+	}
+	if (errormessage != "") {
+		alert(errormessage);
+		return false;
+
+	}
 }
-function validateEmail(email){
+function validateEmail(inputvalue){
 	var emailRegExp = /[\w\.\S]+@\w+\.\S+/igm;
-	return
+	return emailRegExp.test(inputvalue);
 }
-function validatePhoneNumber() {
+function validatePhoneNumber(inputvalue) {
 	var phoneRegExp =/\(?(\d{3})\)?[\s-]?\d{3}[\s-]?\d{4}/igm;
+	return phoneRegExp.test(inputvalue);
 }
-function validateWebsite(){
+function validateWebsite(inputvalue){
 	var websiteRegExp = /www\.(\S+).(com|edu|org|net)/igm;
+	return websiteRegExp.test(inputvalue);
 }
